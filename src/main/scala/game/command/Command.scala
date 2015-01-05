107b231e06944881ff73ed4c4a5253388fc3305a
Louis Li <louisrli@gmail.com>
2015-01-05 03:09:03 -0800

    Document command

--- src/main/scala/game/command/Command.scala
+++ src/main/scala/game/command/Command.scala
@@ -2,6 +2,11 @@ package com.louis.fortypoints.game
 
 import com.louis.fortypoints.card._
 
+/**
+ * Command abstracts a user input (it could come from the console, the web, etc.)
+ *
+ * Only certain game phases require commands to continue.
+ */
 sealed trait Command
 case class SetTrump(player: Int, cards: List[Card]) extends Command
 case class HouseFilterBottomCards(player: Int, cards: List[Card]) extends Command
