715dd53c0421f053f8d890768828f95082b6cdf6
Louis Li <louisrli@gmail.com>
2015-01-06 01:29:22 -0800

    Print the board a little more prettily.

--- src/main/scala/game/console/ConsoleUtil.scala
+++ src/main/scala/game/console/ConsoleUtil.scala
@@ -23,7 +23,7 @@ object ConsoleUtil {
       case HouseBottomFilter => "Bottom cards: " + state.deck.cards + "\n" + currentHand
       case HouseCallCards => "[UNIMPLEMENTED] Skipping house call cards for now..." // TODO(louisli)
       case RoundFirstTurn | RoundOtherTurn => 
-        List(currentHand, "Board: " + state.getPlays.toString) mkString "\n" // TODO(louisli) print prettily
+        List(currentHand, "Board: " + state.getPlays mkString " ") mkString "\n" // TODO(louisli) print prettily
       case RoundEnd => "Ending the round" // TODO(louisli) to print the winner, this should really be after
       case CountPoints => "[UNIMPLEMENTED] Count points"
       case GameEnd => "Game end"
